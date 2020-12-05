30ce1fa573d363f3a7f572c90de3b9998055b342
Rulof Burger <rulof@predictiveinsights.net>
2020-12-05 15:34:28 +0200

    Add educ spline to script

--- scripts/run_lasso.R
+++ scripts/run_lasso.R
@@ -2,16 +2,17 @@ library(caret)
 library(tidyverse)
 
 
-# weights_plus_splines <- haven::read_dta("data/weights_plus_splines.dta") %>% 
-#   tibble %>% 
+# weights_plus_splines <- haven::read_dta("D:/Dropbox/My Documents/Research/AA/UNU-WIDER project/weights_plus_splines.dta") %>%
+#   tibble %>%
 #   select(
-#     l_imputed_real, race, age_20, age_25, age_30, age_35, age_40, age_45, age_50, age_55,
+#     l_imputed_real, race, educ_10, educ_11, educ_12, educ_13, educ_15, 
+#     age_20, age_25, age_30, age_35, age_40, age_45, age_50, age_55,
 #     year_1997, year_1998, year_1999, year_2000, year_2001, year_2002, year_2003, year_2004,
-#     year_2005, year_2006, year_2007, year_2008, year_2010, year_2011, year_2012, year_2013, 
-#     year_2014, year_2015, year_2016, year_2017
-#   ) %>% 
-#   mutate(white = race == 4) %>% 
-#   filter(!is.na(l_imputed_real)) %>% 
+#     year_2005, year_2006, year_2007, year_2008, year_2010, year_2011, year_2012, year_2013,
+#     year_2014, year_2015, year_2016, year_2017, phi
+#   ) %>%
+#   mutate(white = race == 4) %>%
+#   filter(!is.na(l_imputed_real)) %>%
 #   select(-race)
 # 
 # saveRDS(weights_plus_splines, "data/weights_plus_splines.rds")
